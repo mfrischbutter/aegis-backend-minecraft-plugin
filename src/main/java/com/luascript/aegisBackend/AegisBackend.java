4f8a9d3c4b1306ccff26f3bc418cae1c2e673235
Michael Frischbutter <michael@frischbutter.dev>
2025-10-29 19:30:19 +0100

    init

---
+++ src/main/java/com/luascript/aegisBackend/AegisBackend.java
@@ -0,0 +1,17 @@
+package com.luascript.aegisBackend;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AegisBackend extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
